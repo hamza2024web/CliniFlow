fee9ecde1688a150be8a159d9fe42770ab8fb9cb
hamza2024web <hamzaakroubi1528@gmail.com>
2025-10-13 07:16:11 +0100

    create the methode do get in dashboard file

--- clinique-webapp/src/main/java/com/clinique/webapp/servlet/DashboardServlet.java
+++ clinique-webapp/src/main/java/com/clinique/webapp/servlet/DashboardServlet.java
@@ -5,21 +5,14 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
-@WebServlet("/logout")
-public class LogoutServlet extends HttpServlet{
+@WebServlet("/dashboard")
+public class DashboardServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession(false);
-
-        if (session != null){
-            session.invalidate();
-        }
-
-        response.sendRedirect(request.getContextPath() + "/login");
+        request.getRequestDispatcher("/WEB-INF/views/dashboard.jsp").forward(request,response);
     }
 }
