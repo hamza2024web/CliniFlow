d1962d87cb7a6c9c1385bdebeabc35c4ee74244d
hamza2024web <hamzaakroubi1528@gmail.com>
2025-10-16 09:04:40 +0100

    create the repository implementation for availability class

---
+++ clinique-repository/src/main/java/repository/Interface/AvailabilityRepository.java
@@ -0,0 +1,18 @@
+package repository.Interface;
+
+import com.clinique.domain.Availability;
+import com.clinique.domain.Doctor;
+import com.clinique.domain.Enum.DayOfWeek;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AvailabilityRepository {
+    Availability save(Availability availability);
+    Optional<Availability> findById(Long id);
+    List<Availability> findAll();
+    List<Availability> findByDoctor(Doctor doctor);
+    Optional<Availability> findByDoctorAndDay(Doctor doctor , DayOfWeek dayOfWeek);
+    void delete(Long id);
+    List<Availability> findActiveByDoctor(Doctor doctor);
+}
